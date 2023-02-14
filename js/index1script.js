e0cbd8b696618877c589fb4e726ffaec68991d24
Himanshu Singh <himanshuich20@gmail.com>
2023-02-14 22:41:25 +0530

    Add files via upload

---
+++ js/index1script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $('#productManagerCarousel').slick({
+    slidesToShow: 1,
+    dots:true,
+    centerMode: true,
+  });
+});
+
+$(document).ready(function(){
+  $('#sampleResumeCarousel').slick({
+    slidesToShow: 1,
+    dots:true,
+    centerMode: true,
+  });
+});
+
+$(document).ready(function() {
+  $('#productListCarousel').slick({
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    dots:true,
+  });
+})
